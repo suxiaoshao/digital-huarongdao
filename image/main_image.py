d22441f508d9eebdf963f8ebe6b12cf443e189b2
suxiaoshao <2108412077@qq.com>
2020-10-05 13:11:45 +0800

    fix get_serial_number bug

--- image/main_image.py
+++ image/main_image.py
@@ -3,7 +3,6 @@ import json
 from base64 import b64decode
 from typing import List
 from image import image
-from util import compare
 
 
 class MainImage(image.MyImage):
@@ -15,13 +14,13 @@ class MainImage(image.MyImage):
         self.uuid: str = info_data['uuid']
         super(MainImage, self).__init__(b64decode(info_data['img']))
 
-    def to_get_serial_number(self, other_image: image.MyImage) -> List[int]:
+    def to_get_serial_number(self, other_image: image.MyImage) -> [List[int]]:
         flag_list: List[int] = [0] * len(self.cut_image)
         flag = 0
         for i, self_cuy_image in enumerate(self.cut_image):
             for j, other_cut_image in enumerate(other_image.cut_image):
-                if compare.classify_a_hash(self_cuy_image, other_cut_image) == 0:
-                    flag_list[i] = j
+                if (self_cuy_image == other_cut_image).all():
+                    flag_list[i] = j + 1
                     flag += 1
                     break
-        return flag_list if flag == len(self.cut_image) else []
+        return flag_list if flag == len(self.cut_image) - 1 else []
